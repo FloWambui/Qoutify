e2995982573938fcc8854ea9ed59ace1e7bc5ea7
FloWambui <gflorencewambui@gmail.com>
2022-04-11 12:47:26 +0300

    this commit will add the class and the qoutes directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Qoutify';
+  qoutes :Quote [] =[
+    {id:1, name:'"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""'},
+    {id:1, name:'"The fear of God is the beginning of Wisdom!"'},
+    {id:1, name:'"We fear the future because we are wasting today."'},
+    {id:1, name:'"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."'},
+  ];
 }
