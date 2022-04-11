f338de7c008fd7b8ea54508da9a17f32ef89674c
FloWambui <gflorencewambui@gmail.com>
2022-04-11 13:05:22 +0300

    this commit will nest the quote component to the root component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Quote } from './quote'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  qoutes :Quote [] =[
-    {id:1, name:'"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""'},
-    {id:1, name:'"The fear of God is the beginning of Wisdom!"'},
-    {id:1, name:'"We fear the future because we are wasting today."'},
-    {id:1, name:'"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."'},
-  ];
 }
