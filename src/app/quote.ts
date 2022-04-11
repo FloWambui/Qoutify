6450ba279b3206111496e3420a18e4d4c460b393
FloWambui <gflorencewambui@gmail.com>
2022-04-11 17:54:19 +0300

    ft:this commit will add a date pipe

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription:boolean;
-    constructor(public id:number, public quote: string, public author: string, public name:string,){
+    constructor(public id:number, public quote: string, public author: string, public name:string, public publishDate: Date){
         this.showDescription=false;
     }
     // id!: number;
