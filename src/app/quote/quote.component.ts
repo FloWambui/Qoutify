76f6699c70ee3c510a4785b77d33e38519b01e6e
FloWambui <gflorencewambui@gmail.com>
2022-04-11 23:19:42 +0300

    ft:this commit will add the add quote functionality

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,18 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   qoutes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'Flo Wambui', new Date(2020, 3, 14)),
-    new Quote(2, '"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""', 'Eric Thomas', 'Flo Wambui', new Date(2022, 0, 1)),
+    new Quote(1, '"Keep on going, and the chances are that you will stumble on something,perhaps when you are least expecting it. I never heard of anyone ever stumbling on something sitting down."', 'Charles F. Kettering', 'Flo Wambui', new Date(2020, 3, 14)),
+    new Quote(2, '"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!" "', 'Eric Thomas', 'Flo Wambui', new Date(2022, 0, 1)),
     new Quote(3, '"The fear of God is the beginning of Wisdom!"', 'King Solomon', 'Flo Wambui', new Date(1600, 0, 1)),
     new Quote(4, '"We fear the future because we are wasting today."', 'Mother Teresa', 'Flo Wambui', new Date(1997, 8, 5)),
     new Quote(5, '"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."', 'Muammar al-Gaddafi', 'Flo Wambui', new Date(2011, 9, 25)),
   ];
+  addNewQuote (quote:any){
+    let quoteLength = this.qoutes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.publishDate)
+    this.qoutes.push(quote)
+  }
   toggleDetails(index: any) {
     this.qoutes[index].showDescription = !this.qoutes[index].showDescription;
   };
