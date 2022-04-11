ce2338c754e678286e08faf8445822aafdc3bbc8
FloWambui <gflorencewambui@gmail.com>
2022-04-11 13:14:49 +0300

    this commit will add the qoute author's name

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,6 @@
 export class Quote {
     id!: number;
     name!: string;
-    // author: string;
+    author!: string;
+    // writer!: string;
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   qoutes: Quote[] = [
-    { id: 1, name: '"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""' },
-    { id: 1, name: '"The fear of God is the beginning of Wisdom!"' },
-    { id: 1, name: '"We fear the future because we are wasting today."' },
-    { id: 1, name: '"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."' },
+    { id: 1, name: '"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""', author:'Eric Thomas' },
+    { id: 2, name: '"The fear of God is the beginning of Wisdom!"', author:'King Solomon'},
+    { id: 3, name: '"We fear the future because we are wasting today."', author:'Mother Teresa' },
+    { id: 4, name: '"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."', author:'Muammar al-Gaddafi'},
   ];
 
   constructor() { }
