0aafbce1497cf7e90442639f3f9e32b5a29bd018
FloWambui <gflorencewambui@gmail.com>
2022-04-11 14:31:33 +0300

    ft:this commit will add the showDescription constructor to the quote.ts file and add the corresponding properties of id,quote,author and name

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,10 @@
 export class Quote {
-    id!: number;
-    name!: string;
-    author!: string;
+    showDescription:boolean;
+    constructor(public id:number, public quote: string, public author: string, public name:string,){
+        this.showDescription=false;
+    }
+    // id!: number;
+    // name!: string;
+    // author!: string;
     // writer!: string;
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   qoutes: Quote[] = [
-    { id: 1, name: 'Watch finding Nemo', author: 'Find an online version and watch merlin find his son' },
-    { id: 1, name: '"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""', author: 'Eric Thomas' },
-    { id: 2, name: '"The fear of God is the beginning of Wisdom!"', author: 'King Solomon' },
-    { id: 3, name: '"We fear the future because we are wasting today."', author: 'Mother Teresa' },
-    { id: 4, name: '"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."', author: 'Muammar al-Gaddafi' },
+   new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'Flo Wambui'),
+   new Quote (1,'"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""', 'Eric Thomas', 'Flo Wambui'),
+   new Quote(2, '"The fear of God is the beginning of Wisdom!"', 'King Solomon', 'Flo Wambui' ),
+   new Quote (3, '"We fear the future because we are wasting today."','Mother Teresa', 'Flo Wambui'),
+   new Quote (4, '"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."', 'Muammar al-Gaddafi' , 'Flo Wambui'),
   ];
 
   constructor() { }
