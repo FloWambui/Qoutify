c2d38d5dcaf4972e73315bb920361f81f4a9a7fc
FloWambui <gflorencewambui@gmail.com>
2022-04-11 18:33:21 +0300

    ft:this commit will add the logic for deleting a quote

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -12,7 +12,7 @@ export class QuoteDetailComponent implements OnInit {
   quote!: Quote;
   @Output() isComplete = new EventEmitter<boolean>();
 
-  quoteComplete(complete:boolean){
+  quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,18 +8,22 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   qoutes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'Flo Wambui',new Date(2020, 3, 14)),
-    new Quote(2, '"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""', 'Eric Thomas', 'Flo Wambui',new Date(2022, 0, 1)),
-    new Quote(3, '"The fear of God is the beginning of Wisdom!"', 'King Solomon', 'Flo Wambui',new Date(1600, 0, 1)),
-    new Quote(4, '"We fear the future because we are wasting today."', 'Mother Teresa', 'Flo Wambui',new Date(1997, 8, 5)),
-    new Quote(5, '"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."', 'Muammar al-Gaddafi', 'Flo Wambui',new Date(2011, 9, 25)),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'Flo Wambui', new Date(2020, 3, 14)),
+    new Quote(2, '"Live Everyday a Happy Life. Several years down the line, You will look back and say "I lived!""', 'Eric Thomas', 'Flo Wambui', new Date(2022, 0, 1)),
+    new Quote(3, '"The fear of God is the beginning of Wisdom!"', 'King Solomon', 'Flo Wambui', new Date(1600, 0, 1)),
+    new Quote(4, '"We fear the future because we are wasting today."', 'Mother Teresa', 'Flo Wambui', new Date(1997, 8, 5)),
+    new Quote(5, '"Once a rular becomes religious,it becomes impossible for you to debate with him. Once someone rules in the name of religion, your lives become hell."', 'Muammar al-Gaddafi', 'Flo Wambui', new Date(2011, 9, 25)),
   ];
   toggleDetails(index: any) {
     this.qoutes[index].showDescription = !this.qoutes[index].showDescription;
   };
-  completeQuote(isComplete: any, index: any) {
+  deleteQuote(isComplete: any, index: any) {
     if ('isComplate') {
-      this.qoutes.splice(index, 1);
+      let toDelete = confirm(`${this.qoutes[index].name}, are you sure you want to delete this quote ?`)
+      if (toDelete) {
+        this.qoutes.splice(index, 1);
+      }
+
     }
   }
   constructor() { }
